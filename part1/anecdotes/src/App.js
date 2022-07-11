d8100f3e97033e39e4492014ee7da69b211fd713
fersayago <sayago.fernando@gmail.com>
2022-07-10 23:32:27 -0300

    add: 1.13*: anecdotes step2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,16 +10,26 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
   const [selected, setSelected] = useState(0)
 
   const randomizeQuote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
+  const voteQuote = () => {
+    const newPoints = [ ...points ]
+    newPoints[selected] += 1
+    setPoints(newPoints)
+  }
+
   return (
     <div>
       {anecdotes[selected]}
       <br />
+      has {points[selected]} votes
+      <br />
+      <button onClick={voteQuote}>Vote</button>
       <button onClick={randomizeQuote}>next anecdote</button>
     </div>
   )
