3957018780a006063c6d319005eb1eaa392ddc75
fersayago <sayago.fernando@gmail.com>
2022-07-21 10:59:39 -0300

    add: 2.3*: Course information step8

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import Header from './Header'
-import Content from './Content'
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
 
 const Course = (props) => {
 
-  let total = 0;
+  const sumOfExercises = props.course.parts.reduce(
+    (prevValue, currValue) => prevValue + currValue.exercises,
+    0
+  );
 
-  props.course.parts.forEach(part => {
-    total += part.exercises;
-  })
-  
   return (
     <div>
       <Header course={props.course} />
       <Content parts={props.course.parts} />
-      <p><b>total of {total} exercises</b></p>
+      <p>
+        <b>total of {sumOfExercises} exercises</b>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Course
+export default Course;
